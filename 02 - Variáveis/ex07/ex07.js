df6b7ee127bc085edb786875f4552b5938707a52
NiloDial <nilodias97@gmail.com>
2024-09-28 20:08:28 -0300

    Nome das viriáveis

---
+++ 02 - Variáveis/ex07/ex07.js
@@ -0,0 +1,37 @@
+//Nome das variáveis
+
+//Case sensitive
+var username = "João";
+var userName = "Paula";
+
+console.log(username);
+console.log(userName);
+
+//Pode usar caracteres especiais
+var ação = "Começar";
+
+console.log(ação);
+
+//Não pode começar com númeoro
+
+/*
+var 1number = 2
+
+console.log(1number) Resultado vai ser erro
+*/
+
+//Não pode ter espaço
+
+/*
+var = first name = "João"
+
+console.log(first name) Resultado vai ser erro
+*/
+
+// Duas formas de separar o nome da variável
+
+var first_name = "João"; //Usar underline
+var firstName = "Paula"; //Segundo nome da variável com letra maiúscula
+
+console.log(first_name);
+console.log(firstName);
