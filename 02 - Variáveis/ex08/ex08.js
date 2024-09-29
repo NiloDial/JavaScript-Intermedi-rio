66bb769946401a68e9f6eaf1ff46becabb6141a7
NiloDial <nilodias97@gmail.com>
2024-09-29 20:16:29 -0300

    strings

---
+++ 02 - Variáveis/ex08/ex08.js
@@ -0,0 +1,14 @@
+//string
+
+var username = "Rodrigo";
+console.log(username);
+console.log(typeof username);
+
+console.log("Para usar string itilizamos aspas");
+console.log("Tambem podemos usar aspas simples");
+
+console.log('Para conseguir usar as duas usamos primeiro a "aspas simples"');
+console.log("Ou o 'contrário'");
+
+console.log(`Para quebra
+    de linha`);
